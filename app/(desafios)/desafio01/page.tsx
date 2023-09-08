67380f9c2be701b8fb91398a0d62b424eca10ede
Eduardo Lima <eduardolimadesenvolvedor@gmail.com>
2023-09-08 11:34:30 -0300

    work challenge 01

---
+++ app/(desafios)/desafio01/page.tsx
@@ -0,0 +1,11 @@
+import "./style.sass"
+
+export default function Desafio01() {
+    return <section className="container">
+        <header className="header">
+            foobar
+        </header>
+        <main></main>
+    </section>
+}
+
